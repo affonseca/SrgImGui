a74c5a8d7fa2e6530ec43259607d89665c886622
André Fonseca <afcfonseca93@gmail.com>
2024-12-12 16:04:06 +0000

    Simplified and fixed issue with string conversion operations.

--- Source/SrgImGui/Public/SrgImGuiStringConversion.h
+++ Source/SrgImGui/Public/SrgImGuiStringConversion.h
@@ -1,53 +1,8 @@
-// © Surgent Studios
+// Â© Surgent Studios
 
 #pragma once
 
 #include "CoreMinimal.h"
 
-template <typename To, int32 DefaultConversionSize = DEFAULT_STRING_CONVERSION_SIZE, typename From>
-FORCEINLINE const To* StringCastPtr(const From* Str)
-{
-	const To* OutPtr = nullptr;
-	if constexpr (TIsCharEncodingCompatibleWith_V<From, To>)
-	{
-		OutPtr = TStringPointer<To>((const To*) Str).Get();
-	}
-	else
-	{
-		OutPtr = TStringConversion<TStringConvert<From, To>, DefaultConversionSize>(Str).Get();
-	}
-	return OutPtr;
-}
-
-template <typename Interm, typename To, int32 DefaultConversionSize = DEFAULT_STRING_CONVERSION_SIZE, typename From>
-FORCEINLINE const To* StringCastThenReintPtr(const From* Str)
-{
-	const Interm* IntermPtr = nullptr;
-	if constexpr (TIsCharEncodingCompatibleWith_V<From, Interm>)
-	{
-		IntermPtr = TStringPointer<Interm>((const Interm*) Str).Get();
-	}
-	else
-	{
-		IntermPtr = TStringConversion<TStringConvert<From, Interm>, DefaultConversionSize>(Str).Get();
-	}
-	return reinterpret_cast<const To*>(IntermPtr);
-}
-
-template <typename Interm, typename To, int32 DefaultConversionSize = DEFAULT_STRING_CONVERSION_SIZE, typename From>
-FORCEINLINE const To* StringReintThenCastPtr(const From* Str)
-{
-	const To* OutPtr = nullptr;
-	if constexpr (TIsCharEncodingCompatibleWith_V<Interm, To>)
-	{
-		OutPtr = TStringPointer<To>((const To*) Str).Get();
-	}
-	else
-	{
-		OutPtr = TStringConversion<TStringConvert<Interm, To>, DefaultConversionSize>((const Interm*) Str).Get();
-	}
-	return OutPtr;
-}
-
-#define TO_IMGUI(Str) StringCastThenReintPtr<UTF8CHAR, ANSICHAR>(Str)
-#define FROM_IMGUI(Str) StringReintThenCastPtr<UTF8CHAR, TCHAR>(Str)
+#define TO_IMGUI(Str) reinterpret_cast<const ANSICHAR*>(StringCast<UTF8CHAR>(Str).Get())
+#define FROM_IMGUI(Str) StringCast<TCHAR>(Str).Get()
